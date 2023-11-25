67b117782e8c97b6c7e96dec09af00a0fc564988
Samiul Muztaba <samiulmuztaba@outlook.com>
2023-11-25 13:14:50 +0600

    normal commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# year is leap or not
+def is_leap(year):
+    leap = False
+    
+    # Write your logic here
+    if year % 4 == 0:
+        if year % 400 == 0:
+            return True
+        elif year % 100 == 0:
+            return leap
+    else:
+        return leap
+
+year = int(input())
+print(is_leap(year))
